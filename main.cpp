c765656f0f12f45c6e055a72326fb43b3536be1a
sekcayala <aayala39@mail.csuchico.edu>
2018-04-16 18:26:53 -0700

    Too long

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void rearrangeInt (int & , int & , int & );
+
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -8,9 +10,9 @@ int main()
   cout<<"Enter Red, Green, and Blue values: ";
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
-
-
-
+  
+  
+  rearrangeInt (red, green, blue);
 
 
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -18,4 +20,55 @@ int main()
   cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
   return 0;
   //...END OF "DO NOT CHANGE" AREA
+}
+
+void rearrangeInt(int & r, int & g, int & b)
+{
+int greatest;
+int mid;
+int least;
+
+  if (r>=g && r>=b && g>=b)
+   {
+    greatest = r;
+    mid = g;
+    least = b;
+   }
+  else if (r>=g && r>=b && b>=g)
+   {
+   greatest = r;
+   mid = b;
+   least = g;
+   }
+  else if (g>=r && g>=b && r>=b)
+   {
+   greatest = g;
+   mid = r;
+   least = b;
+   }
+ else if (g>=r && g>=b && b>=r)
+   {
+   greatest = g;
+   mid = b;
+   least = r;
+   }
+  else if (b>=r && b>=g && g>=r)
+   {
+   greatest = b;
+   mid = g;
+   least = r;
+   }
+  else if (b>=r && b>=g && r>=g)
+   {
+   greatest = b;
+   mid = r;
+   least = g;
+   }
+  else 
+  {
+  cout<<"ERROR\n";
+  }
+r=greatest;
+g=mid;
+b=least;
 }
